03715f3f9475f2b0f7801f3a6d13317e8f27b80b
Adam K Dean <adamkdean@googlemail.com>
2020-07-03 22:32:07 +0100

    feat: add wifi credentials header

---
+++ src/wifi.h
@@ -0,0 +1,8 @@
+//  __ __  __   __  __   __   __
+// |_ (__ |__) (__)  _) /__  /__
+// |__ __)|    (__) /__ \__) \__)
+//
+// (C) Copyright 2020 Adam K Dean
+
+#define WIFI_SSID "ssidhere"
+#define WIFI_PASSWORD "passwordhere"
