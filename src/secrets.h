d92977f1033e7c7a26afb7f7e3d2df20ca298c9d
Adam K Dean <adamkdean@googlemail.com>
2020-07-04 14:51:05 +0100

    feat: add secrets header

---
+++ src/secrets.h
@@ -0,0 +1,10 @@
+//  __ __  __   __  __   __   __
+// |_ (__ |__) (__)  _) /__  /__
+// |__ __)|    (__) /__ \__) \__)
+//
+// (C) Copyright 2020 Adam K Dean
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+#define DATA_URL "http://"
+#define DATA_BEARER_TOKEN ""
